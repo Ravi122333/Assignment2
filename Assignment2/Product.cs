4e45d99a816e533f30e2d9fbb5c3a8bf80b22694
Ravikumar7676 <gangiseetyravikumar@gmail.com>
2024-05-31 00:39:26 -0400

    This is First Commit with Product class

---
+++ Assignment2/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Product
+    {
+        // Properties
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; private set; }
+
+        // Constructor
+        public Product(int productID, string productName, decimal price, int stock)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        // Methods
+        public void IncreaseStock(int quantity)
+        {
+            Stock += quantity;
+        }
+
+        public void DecreaseStock(int quantity)
+        {
+            if (Stock >= quantity)
+            {
+                Stock -= quantity;
+            }
+            else
+            {
+                throw new InvalidOperationException("Insufficient stock.");
+            }
+        }
+    }
+}
